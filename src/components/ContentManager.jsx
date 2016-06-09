c9a2ac8e0eb4f6222544570de3c97a77af2f6e1c
vherever <vherever@gmail.com>
2016-06-09 17:16:33 +0300

    structure

---
+++ src/components/ContentManager.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var ContentManager = React.createClass({
+    render: function() {
+        return (
+            <div className="contentManager">
+                greetings from main
+            </div>
+        );
+    }
+});
+
+module.exports = ContentManager;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ContentManager = require('./components/ContentManager.jsx');
+
+ReactDOM.render(<ContentManager />, document.getElementById('content'));
